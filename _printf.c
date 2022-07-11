87e7dbd3b4ddf7f4a345a0b6505cfb4976a4bdd9
yeshan111 <yeshan111@gmail.com>
2022-07-11 03:08:53 -0700

    srrfg

--- _printf.c
+++ _printf.c
@@ -1,71 +1,80 @@
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
-
 /**
- * identifier - identifies the _printf specifier
- * @a: specifier
- * @arg: argument
- * Return: symbol to be used
- */
-int (*identifier(const char *format))(va_list)
+  * find_function - function that finds formats for _printf
+  * @format: format (char, string, int, dec)
+  * Return: 0
+  */
+
+int (*find_function(const char *format))(va_list)
 {
-	unsigned int c = 0;
-
-
-	spec id[] = {
-			{"c", print_char},
-			{"s", print_str},
-			{NULL, NULL}
+	unsigned int i = 0;
+	code_f find_f[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"d", print_dec},
+		{"b", print_bin},
+		{"u", print_unsig},
+		{"o", print_octal},
+		{"x", print_x},
+		{"X", print_X},
+		{"r", print_rev},
+		{"R", print_rot13},
+		{NULL, NULL}
 	};
 
-	while (id[c].sym)
+	while (find_f[i].sc)
 	{
-		if (id[c].sym[0] == (*format))
-			return (id[c].p(arg));
+		if (find_f[i].sc[0] == (*format))
+			return (find_f[i].f);
 		i++;
-		return (NULL);
 	}
+	return (NULL);
 }
 
-
 /**
- * _printf - mimics the printf function
- * @format: character string
- * Return: output according to the format
- */
+  * _printf - function to produce output
+  * @format: format (char, string, int, dec)
+  * Return: size of output text
+  */
+
 int _printf(const char *format, ...)
 {
-	int (*p)(va_list);
-	va_list arg;
-	int i = 0, charprinted = 0;
+	va_list list;
+	int (*f)(va_list);
+	unsigned int i = 0, cprint = 0;
 
-	va_start(arg, format);
-	while(format[i])
+	if (format == NULL)
+		return (-1);
+	va_start(list, format);
+	while (format[i])
 	{
-		while(format[i] != '%' && format[i])
+		while (format[i] != '%' && format[i])
 		{
 			_putchar(format[i]);
-			charprinted++;
+			cprint++;
 			i++;
 		}
-
-		if(format[i] != '\0')
-			return(charprinted);
-		p = identifier(&format[i + 1]);
-		if (p != NULL)
+		if (format[i] == '\0')
+			return (cprint);
+		f = find_function(&format[i + 1]);
+		if (f != NULL)
 		{
-			charprinted += p(arg);
+			cprint += f(list);
 			i += 2;
 			continue;
 		}
 		if (!format[i + 1])
 			return (-1);
 		_putchar(format[i]);
-		charprinted++;
+		cprint++;
 		if (format[i + 1] == '%')
-			i =+ 2;
+			i += 2;
 		else
 			i++;
 	}
-	va_end(arg);
-	return (charprinted);
+	va_end(list);
+	return (cprint);
 }
--- main.h
+++ main.h
@@ -1,19 +1,34 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list c);
-int print_str(va_list s);
+int print_string(va_list s);
+int print_int(va_list i);
+int print_dec(va_list d);
+int print_bin(va_list b);
+int print_unsig(va_list u);
+int print_octal(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_rev(va_list r);
+int print_rot13(va_list R);
 
-typedef struct specifier
+/**
+  * struct code_format - Struct format
+  * @sc: The specifiers
+  * @f: The function associated
+  */
+
+typedef struct code_format
 {
-	char *sym;
-	int (*p)(va_list);
-} spec;
+	char *sc;
+	int (*f)(va_list);
+} code_f;
+
 #endif
